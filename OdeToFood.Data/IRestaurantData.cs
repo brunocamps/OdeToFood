89ab904bcd08614d10df81126f2ddf87096b9646
Bruno Campos <bruno.mcamps@outlook.com>
2021-01-08 21:51:56 -0300

    Configured IRestaurantData and added singleton on Startup.cs

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -6,18 +6,31 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetAll(); // method to get all restaurants
     }
 
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        public InMemoryRestaurantData()
+    public class InMemoryRestaurantData : IRestaurantData //implement IRestaurantData
+    { //implement interface shortcut
+
+        //List approach to load restaurants will only work on development mode
+        List<Restaurant> restaurants; 
+        public InMemoryRestaurantData() //constructor
         {
+            restaurants = new List<Restaurant>()
+            {
+                //Warning: a list is not thread safe. It won't be able to process multiple requests.
+                //populate the list with manual data 
+                new Restaurant {Id = 1, Name = "Scott's pizza", Location="Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Cinnamon Club", Location="London", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 3, Name = "Food palace", Location="Virginia", Cuisine = CuisineType.Mexican}
+            };
 
         }
         public IEnumerable<Restaurant> GetAll()
         {
-            
+            return from r in restaurants
+                   orderby r.Name  
+                   select r;
         }
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -21,9 +22,10 @@ namespace OdeToFood
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) //services and components needed
         {
             services.AddRazorPages();
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>(); //only for development and test
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
