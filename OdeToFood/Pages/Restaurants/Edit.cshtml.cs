7fc89e8f447fc2e709c6bcce5ae7d43ee256bab7
brunocamps <bruno.mcamps@outlook.com>
2021-01-18 15:01:28 -0300

    * Edit.cshtml: Added Edit page backend
    * Edit.cshtml.cs: Added Edit page frontend

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,15 +13,21 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
 
         public Restaurant Restaurant { get; set; }
 
-        public EditModel(IRestaurantData restaurantData) //inject the data service
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper) //inject the data service
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+            
         }
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = restaurantData.GetById(restaurantId);
             if(Restaurant == null)
             {
